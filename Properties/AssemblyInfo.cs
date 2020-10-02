b6e7082c10f8a773734de922621fa3b5b0421676
John Spray <johnspray74@gmail.com>
2020-10-03 09:51:37 +1300

    Rename solution

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ALASandbox")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Reactive Calculator")]
+[assembly: AssemblyDescription("A spreadsheet style calculator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ALASandbox")]
+[assembly: AssemblyProduct("Reactive Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
