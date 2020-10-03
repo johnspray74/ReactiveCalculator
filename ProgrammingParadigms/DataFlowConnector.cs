57c92605f3b33f57ab662576ed98c0226e9c9dd7
John Spray <johnspray74@gmail.com>
2020-10-04 10:44:15 +1300

    Total new documentation for the IDataFlow programming paradigm. The old documentation was all wrong.

--- ProgrammingParadigms/DataFlowConnector.cs
+++ ProgrammingParadigms/DataFlowConnector.cs
@@ -4,24 +4,50 @@ using Libraries;
 namespace ProgrammingParadigms
 {
     /// <summary>
-    /// A dataflow with a single scalar value with a primitive data type and a "OnChanged' event.
-    /// OR think of it as an event with data, the receivers are able to read the data at any time.
-    /// Or think of it as an implementation of a global variable and an observer pattern, with access to the variable and observer pattern restricted to the line connections on the diagram.
-    /// Unidirectional - every line is one direction implying sender(s) and receiver(s).
-    /// You can have multiple senders and receivers.
-    /// The data is stored in the wire so receivers that don't act on the event can read its value at any time. Receivers cant change the data or send the event. 
-    /// </summary>
-    /// <typeparam name="T">Generic data type</typeparam>
+    /// A data-flow programming paradigm with a single scalar value with a primitive data type.
+    /// The data flow paradigm is meant to be used as a push paradigm (data is pushed from the source to the destination)
+    /// An example usage would be to wire a FileBrowser which is an IDataFlow source , to a FileReaderWriter, which is an IDataFlow destination. When the FileBrowser outputs a filepath, FileReaderWriter knows where it can read in a file.
+    /// Currently is synchronous only (TBD asynchronous version configurable per domain abstraction port)
+    /// OR think of it as an event with data, the receiver may store the data so can read it at any time after the data is sent.
+    /// Unidirectional - every line on the diagram is one direction implying a sender and receiver.
+    /// You can have multiple sources wired to one destination (fanin). The semantics is that the last sender's data is used.
+    /// Analogous to Reactive Extensions without the duality with Iteration - the flow only uses hot observables, and never completes.
+    /// Simple implementation is the IDataFlow interface. The wired source assigns a value to the Data in the interface which calls the 'set' implementaion in the destination.
+    /// The get should not normally be needed in the interface at all - it is not used when data is transferred from source to destination - the receiver should not need to implement it. However it is there to allow the source to see what the current value is. The current value may have been sent by a different source (using fanin).
+    /// Note the source can only be wired to one destination (no fanout). The destination can we wired from multiple sources (fanin allowed)
+    /// Finally note that the source can choose to have a list of IDataFlow port but this is not usual becasue it complicates the sender abstraction. But it would allow the sender to have fanout. Normally fanout is accomplished by the alternative method of using a connector - see below.
+    /// Note that this programming paradigm does not attempt to identify the sender to the receiver. That would create coupling and ALA is a zero coupling architecture.
+    /// </summary>    /// <typeparam name="T">Generic data type</typeparam>
     public interface IDataFlow<T>
     {
         T Data { get; set; }
     }
 
-    public delegate void DataChangedDelegate();
-
     /// <summary>
-    /// A reversed IDataFlow, the IDataFlow pushes data to the destination whereas IDataFlowB pulls data from source.
-    /// However, the DataChanged event will notify the destination when change happens.
+    /// This second methiod of implementing the data-flow programming paradigm (using IDataFlowB) is more complicated, but overcomes a limitation in the C# language in the case where
+    /// a domain abstraction needs to have more than one receive port. C# doesn't allow you to implement IDataFlow twice for a given type. To overcome this C# language limitation, the receiver uses multiple IDataFlowB fields instead of implementing IDataFlowB.
+    /// So the implemetation is the reverse of IDataFlow - with IDataFlowB, the sender implements the interface and the receiver requires the interface.
+    /// IDataFlowB also allows the receiver to have a 'list port', a port to which multiple senders can be wired in an explicit order so that the receiver can know which sender has sent data.
+    /// The interface itself is implemented with a get. The receiver can get the data at any time, but we want to keep the 'push' semantics of teh paradigm, so C# event is included in teh interface to notify the receiver when the data changes.
+    /// The receiver domain abstraction code must register a handler on this C# event. This needs to be done at initialization time.
+    /// To do that, the receiver domain abstraction should have a private function called xxxPostWiringInitialize(). xxx should be exactly the name of the IDataFlowB field.
+    /// When the receiver is wired, xxxPostWiringInitialize will be called by the wiring engine.  e.g. always use this pattern of three lines of code when using IDataFlowB:
+    /// 
+    /// private IDataFlowB xxx;  // receive port 
+    /// private void xxxPostWringInitialize() {xxx.DataChanged += xxxHandler;}
+    /// private xxxHandler() { something = xxx.Data }
+    /// 
+    /// The sender code looks like this:
+    /// 
+    /// public event DataChangedDelegate DataChanged;  // implement IDataFlowBs event
+    /// T IDataFlowB&ltT&gt.Data { get &eq&gt Data; }  // implement the get
+    /// DataChanged?.Invoke();    // notify destination to get the new data
+    /// 
+    /// Note however that the sender can only implement one IDataFlow of a given type. You can't have the above three lines of code twice in one domain abstraction. So you can't have multiple send ports using IdataFlowB of a given type.
+    /// For this reason, domain abstractions that are senders usually do not use IDataFlowB, they use IDataFlow fields. 
+    /// To wire a sender that uses an IDataFlow field to a receiver that implements an IDataFlow is straighforward.
+    /// To wire a sender that uses an IDataFlow field to a receiver that uses an IDataFlowB field, simply use a connector (see below).
+    /// So unfortunately, when wiring up instances of two abstraction with this IDataFlow data-flow programming paradigm, you must know if the receiver implements IEvent or has a field of type IEventB. If the latter, wire the sender to an instance of a connector, then wire the receiver to the same connector.
     /// </summary>
     /// <typeparam name="T">Generic data type</typeparam>
     public interface IDataFlowB<T>
@@ -29,10 +55,16 @@ namespace ProgrammingParadigms
         T Data { get; }
         event DataChangedDelegate DataChanged;
     }
+    public delegate void DataChangedDelegate();
+
 
     /// <summary>
-    /// It fans out data flows by creating a list and assign the data to the element in the list.
-    /// Moreover, any IDataFlow and IDataFlowB can be transferred bidirectionally.
+    /// The primary reason for the existence of the DataFlowConnector is discussed above in the explanation of the IDataFlowB Interface.
+    /// 1. It allows a sender that has an IDataFlow field to be wired to a receiver that has an IDataFlowB field.
+    /// However it also solves several other use cases.
+    /// 2. It allows a sender that does not support fanout to be wired to multiple receivers (because the connector supports fanout). Furthermore, the receivers can be a mix of ones that implement IDataFlow and ones that have IDataFlowB fields.
+    /// 3. Or think of it as an implementation of a semi-global variable, with the scope of the variable being the line connections on the diagram to and from the connector.
+    /// The data is stored in the connector so senders can change its value at any time and receivers can read its value at any time. 
     /// </summary>
     /// <typeparam name="T">Generic data type</typeparam>
     public class DataFlowConnector<T> : IDataFlow<T>, IDataFlowB<T>
--- ProgrammingParadigms/EventConnector.cs
+++ ProgrammingParadigms/EventConnector.cs
@@ -5,12 +5,12 @@ namespace ProgrammingParadigms
 {
     /// <summary>
     /// Events or observer pattern (publish/subscribe) without data.
-    /// An example usage would be to wire a Button, which is an iEvent sender, to a FileBrowser, which is an IEvent receiver. When the button is pressed, the fileBrowser opens.
+    /// An example usage would be to wire a Button, which is an IEvent sender, to a FileBrowser, which is an IEvent receiver. When the button is pressed, the FileBrowser opens.
     /// Currently is synchronous only (TBD asynchronous version configurable per domain abstraction port)
     /// Analogous to Reactive Extensions without the duality with Iteration - the flow only uses hot observables, and never completes.
     /// Simple implementation is the IEvent interface. Wired sender calls a synchronous method (called Execute). Wired receiver implements a synchronous method (called Execute)
-    /// Note the sender can only be wired to one receiver (no fanout). The receiver can we wired to multiple senders (fanin allowed)
-    /// Finally note that the sender can choose to have a list of event ports but this is not usual becasue it complicates the sender abstraction. But it would allow the sender to have fanout. Normally fanout is accomplished by the alternative method of using a connecter - see below.
+    /// Note the sender can only be wired to one receiver (no fanout). The receiver can we wired from multiple senders (fanin allowed)
+    /// Finally note that the sender can choose to have a list of event port but this is not usual becasue it complicates the sender abstraction. But it would allow the sender to have fanout. Normally fanout is accomplished by the alternative method of using a connector - see below.
     /// Note that unlike C# events, this programming paradigm does not attempt to identify the sender to the receiver. That would create coupling and ALA is a zero coupling architecture.
     /// </summary>
     public interface IEvent
@@ -23,13 +23,13 @@ namespace ProgrammingParadigms
 
 
     /// <summary>
-    /// This second methiod of implementing the Event programming paradigm (using IEventB) is more complicated, but overcomes a limitation in the C# language in the cae where
+    /// This second methiod of implementing the Event programming paradigm (using IEventB) is more complicated, but overcomes a limitation in the C# language in the case where
     /// a domain abstraction needs to have more than one receive port. (C# doesn't allow you to implement IEvent twice) To overcome this C# language limitation, the receiver has IEventB fields instead of implementing IEvent.
     /// So the implemetation is the reverse of IEvent - with IEventB, the sender implements the interface and the receiver requires the interface.
     /// The interface itself is implemented as a C# event. So don't confuse the ALA programming paradigm, IEvent and IEventB, with C# events. IEventB uses a C# event, IEvent uses a simple Execute method.
     /// The receiver domain abstraction code must register a handler on this C# event. This needs to be done at initialization time.
     /// To do that, the receiver domain abstraction should have a private function called xxxPostWiringInitialize(). xxx should be exactly the name of the IEventB field.
-    /// When the reciever is wired, xxxPostWiringInitialize will be called by the wiring engine.  e.g. always use this pattern of three lines of code when using IEventB:
+    /// When the receiver is wired, xxxPostWiringInitialize will be called by the wiring engine.  e.g. always use this pattern of three lines of code when using IEventB:
     /// 
     /// private IEventB xxx;  // receive port 
     /// private void xxxPostWringInitialize() {xxx.EventHappend += xxxHandler;}
@@ -40,11 +40,11 @@ namespace ProgrammingParadigms
     /// public event ExecuteDelegate EventHappened;  // implement IEventB
     /// EventHappened?.Invoke();    // send event one IEventB
     /// 
-    /// Note hoever that the sender can only implement one IEventB. You cant have the above two lines of code twice in one domain abstraction. So it you can't have multiple send ports using iEventB.
+    /// Note however that the sender can only implement one IEventB. You can't have the above two lines of code twice in one domain abstraction. So it you can't have multiple send ports using IEventB.
     /// For this reason, domain abstractions that are senders usually do not use IEventB, they use IEvent fields. 
-    /// To connect a sender that uses an IEventfield to a receiver that uses an IEventB field, simple use a connector (see below)
+    /// To wire a sender that uses an IEvent field to a receiver that implements an IEvent is straighforward.
+    /// To wire a sender that uses an IEvent field to a receiver that uses an IEventB field, simply use a connector (see below).
     /// So unfortunately, when wiring up instances of two abstraction with this IEvent programming paradigm, you must know if the reciever implements IEvent or has a field of type IEventB. If the latter, wire the sender to an instance of a connector, then wire the receiver to the same connector.
-    /// 
     /// </summary>
     public interface IEventB
     {
@@ -61,10 +61,10 @@ namespace ProgrammingParadigms
 
 
     /// <summary>
-    /// The primary reason for the existence of the connector is dicussed above in the explanation of the IEventB Interface.
+    /// The primary reason for the existence of the EventCconnector is discussed above in the explanation of the IEventB Interface.
     /// 1. It allows a sender the has an IEvent field to be wired to a receiver that has an IEventB field.
     /// However it also solves several other use cases.
-    /// 2. It allows a sender that does not support fanout to be wired to multiple receivers (because the connector supports fanout). Furthermore, the receiver can be a mix of ones that implement IEvent and ones that have IEventB fields.
+    /// 2. It allows a sender that does not support fanout to be wired to multiple receivers (because the connector supports fanout). Furthermore, the receivers can be a mix of ones that implement IEvent and ones that have IEventB fields.
     /// 3. many Connectors can be chain wired using the complete port to wire each connector to the next in the chain. This allows events to be generated along the chain in an explicit order effectively giving us ordered execution of sets of events. 
     /// </summary>
     public class EventConnector : IEvent, IEventB
