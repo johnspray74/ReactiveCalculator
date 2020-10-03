c27055aad9cddd7bf773117e56ab4ecf62de0a1c
John Spray <johnspray74@gmail.com>
2020-10-04 09:32:19 +1300

    Add full explanation of the IEvent programming paradigm, together with why the IEventB and EventConnector exist in the implementation of this programming paradigm. Previous comments were incorrect. Similar still needs doing for the IDataFlow programming paradigm

--- ProgrammingParadigms/EventConnector.cs
+++ ProgrammingParadigms/EventConnector.cs
@@ -4,35 +4,73 @@ using System.Collections.Generic;
 namespace ProgrammingParadigms
 {
     /// <summary>
-    /// Events or observer pattern (publish/subscribe without data.
-    /// Can be asynchronous or synchronous
-    /// No data, can be bidirectional.
+    /// Events or observer pattern (publish/subscribe) without data.
+    /// An example usage would be to wire a Button, which is an iEvent sender, to a FileBrowser, which is an IEvent receiver. When the button is pressed, the fileBrowser opens.
+    /// Currently is synchronous only (TBD asynchronous version configurable per domain abstraction port)
     /// Analogous to Reactive Extensions without the duality with Iteration - the flow only uses hot observables, and never completes.
+    /// Simple implementation is the IEvent interface. Wired sender calls a synchronous method (called Execute). Wired receiver implements a synchronous method (called Execute)
+    /// Note the sender can only be wired to one receiver (no fanout). The receiver can we wired to multiple senders (fanin allowed)
+    /// Finally note that the sender can choose to have a list of event ports but this is not usual becasue it complicates the sender abstraction. But it would allow the sender to have fanout. Normally fanout is accomplished by the alternative method of using a connecter - see below.
+    /// Note that unlike C# events, this programming paradigm does not attempt to identify the sender to the receiver. That would create coupling and ALA is a zero coupling architecture.
     /// </summary>
     public interface IEvent
     {
         void Execute();
     }
 
-    public delegate void CallBackDelegate();
+
+
+
 
     /// <summary>
-    /// A reversed IEvent. The IEvent pushes event to the destination whereas the IEventB pulls data from source.
-    /// However, the destination will be notified when event happens at source.
+    /// This second methiod of implementing the Event programming paradigm (using IEventB) is more complicated, but overcomes a limitation in the C# language in the cae where
+    /// a domain abstraction needs to have more than one receive port. (C# doesn't allow you to implement IEvent twice) To overcome this C# language limitation, the receiver has IEventB fields instead of implementing IEvent.
+    /// So the implemetation is the reverse of IEvent - with IEventB, the sender implements the interface and the receiver requires the interface.
+    /// The interface itself is implemented as a C# event. So don't confuse the ALA programming paradigm, IEvent and IEventB, with C# events. IEventB uses a C# event, IEvent uses a simple Execute method.
+    /// The receiver domain abstraction code must register a handler on this C# event. This needs to be done at initialization time.
+    /// To do that, the receiver domain abstraction should have a private function called xxxPostWiringInitialize(). xxx should be exactly the name of the IEventB field.
+    /// When the reciever is wired, xxxPostWiringInitialize will be called by the wiring engine.  e.g. always use this pattern of three lines of code when using IEventB:
+    /// 
+    /// private IEventB xxx;  // receive port 
+    /// private void xxxPostWringInitialize() {xxx.EventHappend += xxxHandler;}
+    /// private xxxHandler() { code to handle the event }
+    /// 
+    /// The sender code looks like this:
+    /// 
+    /// public event ExecuteDelegate EventHappened;  // implement IEventB
+    /// EventHappened?.Invoke();    // send event one IEventB
+    /// 
+    /// Note hoever that the sender can only implement one IEventB. You cant have the above two lines of code twice in one domain abstraction. So it you can't have multiple send ports using iEventB.
+    /// For this reason, domain abstractions that are senders usually do not use IEventB, they use IEvent fields. 
+    /// To connect a sender that uses an IEventfield to a receiver that uses an IEventB field, simple use a connector (see below)
+    /// So unfortunately, when wiring up instances of two abstraction with this IEvent programming paradigm, you must know if the reciever implements IEvent or has a field of type IEventB. If the latter, wire the sender to an instance of a connector, then wire the receiver to the same connector.
+    /// 
     /// </summary>
     public interface IEventB
     {
-        event CallBackDelegate EventHappened;
+        event ExecuteDelegate EventHappened;
     }
 
+    public delegate void ExecuteDelegate();
+
+
+
+
+
+
+
+
     /// <summary>
-    /// It fans out IEvent by creating a list and assign the event to the element in the list.
-    /// Moreover, any IEvent and IEventB can be transferred bidirectionally.
+    /// The primary reason for the existence of the connector is dicussed above in the explanation of the IEventB Interface.
+    /// 1. It allows a sender the has an IEvent field to be wired to a receiver that has an IEventB field.
+    /// However it also solves several other use cases.
+    /// 2. It allows a sender that does not support fanout to be wired to multiple receivers (because the connector supports fanout). Furthermore, the receiver can be a mix of ones that implement IEvent and ones that have IEventB fields.
+    /// 3. many Connectors can be chain wired using the complete port to wire each connector to the next in the chain. This allows events to be generated along the chain in an explicit order effectively giving us ordered execution of sets of events. 
     /// </summary>
     public class EventConnector : IEvent, IEventB
     {
         // Properties
-        public string InstanceName;
+        public string InstanceName { get; set; }
 
         // outputs
         private List<IEvent> fanoutList = new List<IEvent>();
@@ -52,6 +90,6 @@ namespace ProgrammingParadigms
         }
 
         // IEventB implementation --------------------------------------
-        public event CallBackDelegate EventHappened;
+        public event ExecuteDelegate EventHappened;
     }
 }
